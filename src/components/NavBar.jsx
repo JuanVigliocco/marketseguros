92cb8eeef8e9c50aa582363c555b8d048da64670
juanjito1992 <juan.vigliocco@gmail.com>
2022-01-08 17:34:42 -0300

    clase navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+export default function NavBar (){
+    return(
+        <>
+        <h1>Bienvenido a Buho Seguros</h1>
+        <div>
+                <li> <a href=""></a> Accidentes Personales</ li >
+                <li> <a href=""></a> Auto</ li >
+                <li> <a href=""></a> Hogar</ li >
+                <li> <a href=""></a> Bolso Protegido</ li >
+                <li> <a href=""></a> Vida</ li >
+        </div>
+        </>
+    )
+
+
+}
