6c929694375955ecea01b7b3395ba18d6f18b60e
juanjito1992 <juan.vigliocco@gmail.com>
2022-01-13 15:31:18 -0300

    clase navbar

---
+++ src/components/CartWidget/CartWidget.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+import carrito from "./carrito2.png"
+
+
+function CartWidget () {
+    return (
+        <>
+       
+            <img className="imgcarrito" width="" height="35px" src={carrito} alt=""/>
+       </>
+
+
+
+
+
+    )
+}
+
+export default CartWidget
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,34 +1,36 @@
 import React from "react";
 import {Navbar, Nav, NavDropdown, Container} from "react-bootstrap"
-import CartWidget from "./CartWidget/carrito.png"
-
+import CartWidget from "./CartWidget/CartWidget";
 
 
 export default function NavBar ({arrayDeLink}){
     return(
         <>
-<Navbar bg="light" expand="lg">
+  <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
   <Container>
-    <Navbar.Brand href="#home">JV</Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="me-auto">
-        <Nav.Link href="#home">Inicio</Nav.Link>
-          <NavDropdown title="Seguros" id="basic-nav-dropdown">
-          <NavDropdown.Item href="#action/3.1">Auto</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.2">Moto</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.3">Vida</NavDropdown.Item>
-          <NavDropdown.Divider />
-          <NavDropdown.Item href="#action/3.4">Cotizar</NavDropdown.Item>
-        </NavDropdown>
-        <Nav.Link href="#link">Inmobiliaria</Nav.Link>
-        <Nav.Link href="#link">Sobre Nosotros</Nav.Link>
-        <Nav.Link href="#link">Contacto</Nav.Link>
-        <Nav.Link href="#link">Log In</Nav.Link>
-      </Nav>
-    </Navbar.Collapse>
+  <Navbar.Brand href="#home">JV</Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="me-auto">
+      <Nav.Link href="#features">Inicio</Nav.Link>
+      <Nav.Link href="#pricing">Inmobiliaria</Nav.Link>
+      <NavDropdown title="Seguros" id="collasible-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Auto</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Moto</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Vida</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4">Cotizar</NavDropdown.Item>
+      </NavDropdown>
+      <Nav.Link href="#pricing">Contacto</Nav.Link>
+    </Nav>
+    <Nav>
+      <Nav.Link href="#deets">Carrito  <CartWidget/></Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
   </Container>
+  
 </Navbar>
+
         </>
     )
 
