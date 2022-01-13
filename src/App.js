dd7ce2a6532f4906888ce6163280ed500a3842e9
juanjito1992 <juan.vigliocco@gmail.com>
2022-01-13 14:38:25 -0300

    clase navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
 import NavBar from './components/NavBar';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 
 function App() {
+
+  
+
   return (
+    <div className='app'>
     <NavBar />
+    <ItemListContainer greetings={"Bienvenidos"}/>
+    </div>
   );
 }
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,19 +1,36 @@
 import React from "react";
+import {Navbar, Nav, NavDropdown, Container} from "react-bootstrap"
+import CartWidget from "./CartWidget/carrito.png"
 
 
-export default function NavBar (){
+
+export default function NavBar ({arrayDeLink}){
     return(
         <>
-        <h1>Bienvenido a Buho Seguros</h1>
-        <div>
-                <li> <a href=""></a> Accidentes Personales</ li >
-                <li> <a href=""></a> Auto</ li >
-                <li> <a href=""></a> Hogar</ li >
-                <li> <a href=""></a> Bolso Protegido</ li >
-                <li> <a href=""></a> Vida</ li >
-        </div>
+<Navbar bg="light" expand="lg">
+  <Container>
+    <Navbar.Brand href="#home">JV</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="me-auto">
+        <Nav.Link href="#home">Inicio</Nav.Link>
+          <NavDropdown title="Seguros" id="basic-nav-dropdown">
+          <NavDropdown.Item href="#action/3.1">Auto</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.2">Moto</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.3">Vida</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="#action/3.4">Cotizar</NavDropdown.Item>
+        </NavDropdown>
+        <Nav.Link href="#link">Inmobiliaria</Nav.Link>
+        <Nav.Link href="#link">Sobre Nosotros</Nav.Link>
+        <Nav.Link href="#link">Contacto</Nav.Link>
+        <Nav.Link href="#link">Log In</Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
         </>
     )
 
 
-}
+}   
